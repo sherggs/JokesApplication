c2eb0b2403c6d4a9641e8621c07d9914d5121bf6
Ademola <sherggs@gmail.com>
2022-10-27 19:24:46 +0100

    new migrations

---
+++ JokesApplication/Data/Migrations/20221027160729_initalsetups.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesApplication.Data.Migrations
+{
+    public partial class initalsetups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
